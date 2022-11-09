b6dafe4d7c8885ef9c3ffc11a77d83183ce57e91
Ayushe <ayushe17@gmail.com>
2022-11-09 17:34:45 -0500

    Changed names for better understanding

---
+++ src/gui/view/NumberListView.java
@@ -0,0 +1,21 @@
+package gui.view;
+import javax.swing.JTextArea;
+
+import model.BasicStatsModel;
+
+public class NumberListView implements View{
+    private JTextArea jtaNumbers;
+    
+    public NumberListView()
+    {
+        jtaNumbers = new JTextArea(10,50);
+        jtaNumbers.setEditable(false);
+    }
+
+    @Override
+    public void update(BasicStatsModel model) {
+        double num = model.getArrayDouble()[model.getArrayDouble().length - 1];
+	    jtaNumbers.append(num + ","); 
+    }
+
+}

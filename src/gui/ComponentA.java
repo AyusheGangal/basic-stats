7878641fe886bd386c361ab1163dc38088814e04
Ayushe <ayushe17@gmail.com>
2022-11-09 13:12:27 -0500

    Created Leaf (files) for Composite Design Pattern

---
+++ src/gui/ComponentA.java
@@ -0,0 +1,14 @@
+package gui;
+
+import gui.view.View;
+import model.BasicStatsModel;
+
+public class ComponentA implements View{
+
+    @Override
+    public void update(BasicStatsModel model) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
---
+++ src/gui/ComponentB.java
@@ -0,0 +1,14 @@
+package gui;
+
+import gui.view.View;
+import model.BasicStatsModel;
+
+public class ComponentB implements View{
+
+    @Override
+    public void update(BasicStatsModel model) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
